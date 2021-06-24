2b8e9fd89155bb9e7fcc45e7efd49ee235f44fb9
guptaprnv <pranavcool1111@gmail.com>
2021-06-24 14:11:10 +0530

    Update promisehandler.js

--- asynchronous/promisehandler.js
+++ asynchronous/promisehandler.js
@@ -1,36 +1,24 @@
-
-function handlePromise(promise) {
-  return 
-  promise
-  .then(value => {
-      return value;
-  })
-  .catch(value => {
-      return value;
-  })
-}
-
-async function makehandler() {
-    let promise1 = new Promise((resolve,reject) => {
-        setTimeout(() => {
-            resolve(2);
-        },3000)
+async function makehandler(promiseArray) {
+  for (ind in promiseArray) {
+    promiseArray[ind] = promiseArray[ind].catch((error) => {
+      return error;
     });
-    let promise2 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject('error 404');
-        },6000)
-    });
-    handlePromise(Promise.all([promise1,promise2]));
-    
-    for(let indresult = 0; indresult < result[0].length; indresult++){
-        if(result[0][indresult] !== undefined) {
-            console.log('promise'+indresult+'response: '+result[0][indresult]);
-        }
-        else{
-            console.log('promise'+indresult+'rejected: '+result[1][indresult]);
-        }
-    }
-}
+  }
+  response = [];
+  await Promise.all(promiseArray).then((values) => {
+    response = values;
+  });
 
-makehandler();
+  console.log(response);
+}
+let promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(2);
+  }, 1000);
+});
+let promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("error 404");
+  }, 2000);
+});
+makehandler([promise1, promise2]);
